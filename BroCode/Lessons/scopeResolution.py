8e468d448dcedc016d749838d12ff12e6ec782da
ryebread-ats <125844907+ryebread-ats@users.noreply.github.com>
2024-11-07 21:32:25 -0500

    started scope resolution

--- BroCode/Lessons/scopeResolution.py
+++ BroCode/Lessons/scopeResolution.py
@@ -2,3 +2,22 @@
 #where a variable is visible and accessible
 #scope resolution
 #(LEGB) Local -> Enclose -> Global -> Built-In
+"""
+def func1():
+    a = 1   #variable is local to the function it was called in
+    print(a)
+
+def func2():
+    b = 2
+    print(b)
+"""
+
+def func1():
+    x = 1   #variable is local to the function it was called in
+    print(x)
+
+def func2():
+    x = 2
+    print(x)
+func1()
+func2()
